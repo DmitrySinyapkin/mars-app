5ed545144773c4bf1e1e1556ec2b3899872e4953
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-01-17 18:58:43 +0400

    Исправлен текст About

--- mars-view/src/constants/aboutPageText.js
+++ mars-view/src/constants/aboutPageText.js
@@ -1,6 +1,6 @@
 export const ABOUT_TEXT = [
     'Hi, space explorer!<br>',
-    'Welcome to Mars viewes gallery!<br>',
+    'Welcome to the Mars viewes gallery!<br>',
     'Here you can find photos<br>',
     'taken by three NASA\'s<br>',
     'Curiosity, Opportunity and Spirit rovers.'

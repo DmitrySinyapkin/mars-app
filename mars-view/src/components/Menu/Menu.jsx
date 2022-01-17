257f7c5f0d0105b31982877b01461c02f800301e
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-01-17 15:30:47 +0400

    Добавлен хедер

---
+++ mars-view/src/components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Menu.css';
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <ul className="menu__container">
+                <li className="menu__item">Gallery</li>
+                <li className="menu__item">About</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;

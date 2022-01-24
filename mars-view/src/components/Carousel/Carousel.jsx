48dcf35807dbf8411c148217f02e2eddfa23a1b8
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-01-24 18:00:33 +0300

    Добавлены обработчики на кнопки перехода между картинками

--- mars-view/src/components/Carousel/Carousel.jsx
+++ mars-view/src/components/Carousel/Carousel.jsx
@@ -1,9 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import './Carousel.css';
 import Typewriter from "typewriter-effect";
+import { gsap } from 'gsap';
 
 const Carousel = ({images, currentIndex, isOpen, closeHandler}) => {
-    const [current, setCurrent] = useState(currentIndex);
+    const [current, setCurrent] = useState(0);
+    const [photos, setPhotos] = useState([]);
+    const [clickNext, setClickNext] = useState(false);
+    const [clickPrev, setClickPrev] = useState(false);
+
+    const imgRefs = useRef([]);
+    imgRefs.current = [];
+
+    useEffect(() => {
+        setCurrent(currentIndex);
+        setPhotos(images);
+    }, []);
+
+    useEffect(() => {
+        gsap.to(imgRefs[current], {duration: 1, x: -35, rotationY: 90});
+        gsap.to(imgRefs[current + 1], {duration: 1, delay: 0.5, x: -35, rotationY: 90});
+    }, [clickNext]);
+
+    useEffect(() => {
+        gsap.to(imgRefs[current], {duration: 2, x: 35, rotationY: -90});
+        gsap.to(imgRefs[current - 1], {duration: 2, delay: 1, x: 35, rotationY: -90});
+    }, [clickPrev]);
+
+    const addToRefs = (item) => {
+        if (item && !imgRefs.current.includes(item)) {
+            imgRefs.current.push(item);
+        }
+    }
+
+    const handleNextClick = () => {
+        if (current < photos.length - 1) {
+            setClickNext(!clickNext);
+            setCurrent(current + 1);
+        }
+    }
+
+    const handlePrevClick = () => {
+        if (current > 0) {
+            setClickPrev(!clickPrev);
+            setCurrent(current - 1);
+        }
+    }
 
     return (
         <div className={`carousel ${isOpen && "carousel_opened"}`}>
@@ -13,12 +55,21 @@ const Carousel = ({images, currentIndex, isOpen, closeHandler}) => {
                 </div>
                 <div className="carousel__body">
                     <div className="carousel__container">
-                        <div className="carousel__img">
-                            <img src={images[current].img_src} alt=" " />
+                        <div className="carousel__img-wrapper">
+                            {photos.map((img, index) =>
+                                <div
+                                    className={`carousel__img ${index === current ? "img_current" : index < current ? "img_left" : "img_right"}`}
+                                    key={index}
+                                    ref={addToRefs}
+                                    style={{zIndex: `${index === current ? 300 : index > current ? 300 + images.length - index : 300 + index}`}}
+                                >
+                                    <img src={img.img_src} alt=" " />
+                                </div>
+                            )}
                         </div>
                     </div>
                     <div className="carousel__description">
-                        <Typewriter onInit={(typewriter) => {
+                        {/*<Typewriter onInit={(typewriter) => {
                             typewriter
                                 .typeString(`Earth date: ${images[current].earth_date}<br>`)
                                 .pauseFor(200)
@@ -26,12 +77,14 @@ const Carousel = ({images, currentIndex, isOpen, closeHandler}) => {
                                 .pauseFor(200)
                                 .start()
                             }}
-                        />
+                        />*/}
+                        <div>Earth date: {images[current].earth_date}</div>
+                        <div>Camera: {images[current].camera.full_name}</div>
                     </div>
                 </div>
                 <div className="carousel__nav">
-                    <div className="carousel__prev-btn">&lt;</div>
-                    <div className="carousel__next-btn">&gt;</div>
+                    <div className="carousel__prev-btn" onClick={() => handlePrevClick()}>&lt;</div>
+                    <div className="carousel__next-btn" onClick={() => handleNextClick()}>&gt;</div>
                 </div>
             </div>
         </div>

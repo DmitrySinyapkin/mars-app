b23206e4e242da50716940022e8a83a30fea4866
DmitrySinyapkin <dsinyapkin@mail.ru>
2022-01-27 17:59:24 +0300

    Правки в логике

--- mars-view/src/components/Carousel/Carousel.jsx
+++ mars-view/src/components/Carousel/Carousel.jsx
@@ -4,14 +4,44 @@ import { gsap } from 'gsap';
 
 const Carousel = ({images, currentIndex, isOpen, closeHandler}) => {
     const [current, setCurrent] = useState(0);
+    const [photos, setPhotos] = useState([]);
     const [clickNext, setClickNext] = useState(false);
     const [clickPrev, setClickPrev] = useState(false);
 
     const imgRefs = useRef([]);
     imgRefs.current = [];
 
+    const style = {
+        left_hidden: {
+            left: '-4%',
+            transformOrigin: 'right center',
+            transform: 'rotateY(60deg)',
+            display: 'none',
+        },
+        left: {
+            left: '-4%',
+            transformOrigin: 'right center',
+            transform: 'rotateY(60deg)',
+        },
+        center: {
+            left: '33%', 
+        },
+        right: {
+            right: '-4%',
+            transformOrigin: 'left center',
+            transform: 'rotateY(-60deg)',
+        },
+        right_hidden: {
+            right: '-4%',
+            transformOrigin: 'left center',
+            transform: 'rotateY(-60deg)',
+            display: 'none',
+        }
+    }
+
     useEffect(() => {
         setCurrent(currentIndex);
+        setPhotos([...images]);
     }, []);
 
     useEffect(() => {
@@ -33,15 +63,15 @@ const Carousel = ({images, currentIndex, isOpen, closeHandler}) => {
     useLayoutEffect(() => {
         gsap.to(imgRefs.current[current], {duration: 1, x: '-108.5%', transformOrigin: 'right center', rotationY: 60});
         gsap.to(imgRefs.current[current + 1], {duration: 1, delay: 0.5, x: '-108.5%', rotationY: 0});
-        gsap.to(imgRefs.current[current - 1], {duration: 1, delay: 0.1, visibility: 'hidden'});
-        gsap.to(imgRefs.current[current + 2], {duration: 0.2, delay: 1.5, visibility: 'visible'});
+        gsap.to(imgRefs.current[current - 1], {delay: 0.1, display: 'none'});
+        gsap.to(imgRefs.current[current + 2], {delay: 1.5, display: 'block'});
     }, [clickNext]);
 
     useLayoutEffect(() => {
         gsap.to(imgRefs.current[current], {duration: 1, x: "108.5%", transformOrigin: 'left center', rotationY: -60});
         gsap.to(imgRefs.current[current - 1], {duration: 1, delay: 1, x: "108.5%", rotationY: 0});
-        gsap.to(imgRefs.current[current + 1], {duration: 1, delay: 0.5, visibility: 'hidden'});
-        gsap.to(imgRefs.current[current - 2], {duration: 0.2, delay: 1.5, visibility: 'visible'});
+        gsap.to(imgRefs.current[current + 1], {delay: 0.1, display: 'none'});
+        gsap.to(imgRefs.current[current - 2], {delay: 1.5, display: 'block'});
     }, [clickPrev]);
 
     const addToRefs = (item) => {
@@ -51,7 +81,7 @@ const Carousel = ({images, currentIndex, isOpen, closeHandler}) => {
     }
 
     const handleNextClick = () => {
-        if (current < images.length - 1) {
+        if (current < imgRefs.current.length - 1) {
             setClickNext(!clickNext);
             setCurrent(current + 1);
         }
@@ -73,12 +103,12 @@ const Carousel = ({images, currentIndex, isOpen, closeHandler}) => {
                 <div className="carousel__body">
                     <div className="carousel__container">
                         <div className="carousel__img-wrapper">
-                            {images.map((img, index) =>
+                            {photos.map((img, index) =>
                                 <div
-                                    className={`carousel__img ${index === current ? "img_current" : index < current ? "img_left" : "img_right"}`}
+                                    className="carousel__img"
                                     key={index}
                                     ref={addToRefs}
-                                    style={{visibility: `${index < current - 1 || index > current + 1 ? "hidden" : "visible"}`}}
+                                    style={index < current - 1 ? style.left_hidden : index === current - 1 ? style.left : index === current ? style.center : index === current + 1 ? style.right : style.right_hidden}
                                 >
                                     <img src={img.img_src} alt=" " />
                                 </div>
